fc764815bafd05a6741ceeba809333c8f57bb661
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-10-27 12:26:04 +0200

    Create AWS Lambda Function Resource

--- src/main/java/com/myorg/AwsCdkCanaryLambdaDemoStack.java
+++ src/main/java/com/myorg/AwsCdkCanaryLambdaDemoStack.java
@@ -1,10 +1,12 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.codedeploy.*;
+import software.amazon.awscdk.services.lambda.*;
+import software.amazon.awscdk.services.lambda.Runtime;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class AwsCdkCanaryLambdaDemoStack extends Stack {
     public AwsCdkCanaryLambdaDemoStack(final Construct scope, final String id) {
@@ -14,11 +16,33 @@ public class AwsCdkCanaryLambdaDemoStack extends Stack {
     public AwsCdkCanaryLambdaDemoStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        Function function = Function.Builder.create(this, "CdkLambdaFunction")
+                .code(Code.fromAsset("./lambda-application/target/lambda-application-0.1.jar"))
+                .handler("com.myorg.AwsCdkDemoLambdaHandler")
+                .functionName("myFunction")
+                .runtime(Runtime.JAVA_11)
+                .memorySize(256)
+                .timeout(Duration.minutes(5))
+                .build();
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkCanaryLambdaDemoQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        Version version = function.getCurrentVersion();
+        Alias alias = Alias.Builder.create(this, "alias")
+                .aliasName("prod")
+                .version(version)
+                .build();
+
+        LambdaDeploymentConfig config = LambdaDeploymentConfig.Builder
+                .create(this, "CdkLambdaCustomConfig")
+                .trafficRouting(TrafficRouting.timeBasedCanary(TimeBasedCanaryTrafficRoutingProps.builder()
+                        .interval(Duration.minutes(2))
+                        .percentage(5)
+                        .build()))
+                .build();
+
+        LambdaDeploymentGroup deploymentGroup = LambdaDeploymentGroup.Builder
+                .create(this, "CdkLambdaCanaryDeployment")
+                .alias(alias)
+                .deploymentConfig(config)
+                .build();
     }
 }
